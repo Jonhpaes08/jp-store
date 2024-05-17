1af29f282a3e2165973c1ce9d1bf06fd9fb1a5d6
Jonhvmp <jonhpaz08@gmail.com>
2024-05-17 19:16:04 -0300

    UPDATE

---
+++ jp_score_db.sql
@@ -0,0 +1,18 @@
+INSERT INTO usuarios (
+    id,
+    nome,
+    email,
+    senha,
+    data_cadastro,
+    ultimo_login,
+    status
+  )
+VALUES (
+    id:int,
+    'nome:Jonh Alex Paz de Lima',
+    'email:jonhpaz08@gmail.com',
+    'senha:senhateste',
+    'data_cadastro:',
+    'ultimo_login:',
+    'status:'
+  );
